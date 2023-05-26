6ae53b782dcee11a84be06a85f9e9047ad2a0c27
Sharky <Sharky121@mail.ru>
2023-05-26 08:06:59 +0300

    WIP

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,9 +1,20 @@
-import {ProductInterface} from "@/interfaces/product.interface";
+type ProductType = {
+    productId: string;
+}
 
-const Product = () => {
+const Product = ({ productId }: ProductType) => {
     return (
-        <h1>Title</h1>
+        <h1>{ productId }</h1>
     )
 };
+
+export async function getServerSideProps(context: { query: { id: string; }; }) {
+    const { id } = context.query;
+    return {
+        props: {
+            productId: id,
+        },
+    };
+}
 
 export default Product;
