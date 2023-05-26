df93055fb68a00af139431907555fb2bdd505095
Sharky <Sharky121@mail.ru>
2023-05-26 08:18:31 +0300

    WIP

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,10 +1,54 @@
+import Link from "next/link";
+import Image from 'next/image';
+import {products} from "@/data";
+import Header from "@/components/header/header";
+import FeedbackScreen from "@/components/feedback-screen/feedback-screen";
+import Footer from "@/components/footer/footer";
+
 type ProductType = {
     productId: string;
 }
 
 const Product = ({ productId }: ProductType) => {
+    const product = products[parseInt(productId, 10) - 1];
+
     return (
-        <h1>{ productId }</h1>
+        <>
+            <Header headerStyle="page-header--white"/>
+            <main className="page__main">
+                <div className="container">
+                    <section className="page__breadcrumbs breadcrumbs">
+                        <h2 className="visually-hidden">Хлебная крошка</h2>
+                        <Link className="breadcrumbs__back" id="history-back" href="/">Назад</Link>
+                        <ul className="breadcrumbs__list">
+                            <li className="breadcrumbs__item">
+                                <Link className="breadcrumbs__link" href="/">Главная</Link>
+                            </li>
+                            <li className="breadcrumbs__item">
+                                <Link className="breadcrumbs__link" href="/products">Наша продукция</Link>
+                            </li>
+                            <li className="breadcrumbs__item">
+                                <span className="breadcrumbs__link">{product.title}</span>
+                            </li>
+                        </ul>
+                    </section>
+                    <section className="product">
+                        <h2 className="product__title">{product.title}</h2>
+                        <ul className="product__list">
+                            {
+                                product.photos.map((photo, index) => (
+                                    <li className="product__item" key={index}>
+                                        <Image fill className="product__img" alt="" src={photo}/>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </section>
+                </div>
+                <FeedbackScreen/>
+            </main>
+            <Footer/>
+        </>
     )
 };
 
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -52,7 +52,6 @@ const Products = () => {
             </main>
             <Footer/>
         </>
-
     )
 };
 
